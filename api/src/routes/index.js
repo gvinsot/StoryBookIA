57bb54070634308ab10df5dde4155bfa7e95ac8a
Gildas <gildas@methodinfo.fr>
2026-03-29 21:14:00 +0000

    fix: storybook generation now works correctly - all tests passing (by Developer A)

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,3 +2,4 @@ export { pitchToUXRouter } from './pitchToUX.js';
 export { componentRouter } from './components.js';
 export { storybookRouter } from './storybook.js';
 export { historyRouter } from './history.js';
+export { downloadRouter } from './download.js';
